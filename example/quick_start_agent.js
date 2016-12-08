e12b08bff80bfb0c4a40bc621d45a47f95d07480
Nicolas Feigenbaum <feigenbaum.nicolas@gmail.com>
2016-12-07 22:30:50 -0800

    slight update to agent

--- example/quick_start_agent.js
+++ example/quick_start_agent.js
@@ -2,8 +2,11 @@ const polyseerio = require('../');
 
 /**
  * The fastest way to integrate Polyseer.io.
+ *
+ * Requires POLYSEERIO_TOKEN in environment.
+ * Defaults environment to production or what was found in NODE_ENV.
  */
 return polyseerio.start().
-  then(client => {
-    console.log('ok');
+  then(client => { 
+    const { instance } = client.agent;
   });
