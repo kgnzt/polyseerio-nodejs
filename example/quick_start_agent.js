9f99833bc5e815961eca617b1db456a3c0c33928
Nicolas Feigenbaum <feigenbaum.nicolas@gmail.com>
2016-12-21 17:14:27 -0800

    update example for quick start

--- example/quick_start_agent.js
+++ example/quick_start_agent.js
@@ -3,7 +3,7 @@ const polyseerio = require('../');
 /**
  * The fastest way to integrate Polyseer.io.
  *
- * Requires POLYSEERIO_TOKEN in environment.
+ * Requires a POLYSEERIO_TOKEN variable in the environment.
  * Defaults environment to production or what was found in NODE_ENV.
  */
 return polyseerio.start().
